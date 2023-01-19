b07148e110e9c07223c880f4195ac0c4c616b8fe
Jan Štěch <jan.stech@posteo.net>
2023-01-19 01:17:00 +0100

    Subtitles can be now searched according to video timestamp
    Subtitles are now parsed successfully and can be displayed in the console.
    A temporary debug function is defined in watch.js.

--- js/SubtitlesReader.js
+++ js/SubtitlesReader.js
@@ -35,16 +35,69 @@ function SubtitlesReader(subtitlesFile) {
             self.subtitles.push(new Subtitle(num, timeFrom, timeTo, duration, text))
         })
     }
-
+/*
     this.getPreviousLine = function (timestamp) {
-        //TODO
-    }
+        let currentLineIndex = this.performBinarySearchInSubtitles(timestamp);
 
+        return (currentLineIndex === 0) ? null : this.subtitles[currentLineIndex - 1].text;
+    }
+*/
     this.getCurrentLine = function (timestamp) {
-        //TODO
+        let index = this.performBinarySearchInSubtitles(timestamp);
+        if (index === null) {
+            return null;
+        }
+        return this.subtitles[index].text;
     }
-
+/*
     this.getNextLine = function (timestamp) {
-        //TODO
+        let currentLineIndex = this.performBinarySearchInSubtitles(timestamp);
+        return (currentLineIndex === this.subtitles.length - 1) ? null : this.subtitles[currentLineIndex + 1].text;
+    }
+*/
+    this.performBinarySearchInSubtitles = function (timestamp, bottomIndex = -1, upperIndex = -1, startIndex = -1) {
+        let self = this
+
+        let minIndex = (bottomIndex === -1 ) ? 0 : bottomIndex
+        let maxIndex = (upperIndex === -1) ? self.subtitles.length - 1 : upperIndex
+        let currentIndex = (startIndex === -1) ? Math.floor(self.subtitles.length / 2) : startIndex
+
+        //console.log(minIndex + " --> " + currentIndex + " --> " + maxIndex);
+
+        if (minIndex > maxIndex) {
+            //We are at the index between two subtitles, one of which is previous and the other one the next.
+            //For the current timestamp, there is no subtitle available
+            return null;
+        }
+
+        if (minIndex === maxIndex) {
+            if (self.subtitles[currentIndex].timeFrom < timestamp && self.subtitles[currentIndex].timeTo > timestamp) {
+                return minIndex;
+            } else {
+                //We are at the index between two subtitles, one of which is previous and the other one the next.
+                //For the current timestamp, there is no subtitle available
+                return null;
+            }
+        }
+
+        if (self.subtitles[currentIndex].timeFrom < timestamp) {
+            if (self.subtitles[currentIndex].timeTo > timestamp) {
+                return currentIndex
+            } else {
+                //console.log("low");
+                return this.performBinarySearchInSubtitles(timestamp,
+                    currentIndex + 1,
+                    maxIndex,
+                    currentIndex + Math.max(1, Math.floor((maxIndex - currentIndex) / 2)),
+                )
+            }
+        } else {
+            //console.log("high");
+            return this.performBinarySearchInSubtitles(timestamp,
+                minIndex,
+                currentIndex - 1,
+                currentIndex - Math.max(1, Math.floor((currentIndex - minIndex) / 2)),
+            )
+        }
     }
 }
--- js/watch.js
+++ js/watch.js
@@ -2,6 +2,8 @@ $(function(){
     $("#watch-form").on('submit', loadVideo)
 });
 
+var subtitlesReader;
+
 function loadVideo(event)
 {
     event.preventDefault();
@@ -13,9 +15,14 @@ function loadVideo(event)
     $("#video").get(0).load()
     
     let subtitlesFile = $("#subtitles-input").get(0).files[0]
-    let subtitlesReader = new SubtitlesReader(subtitlesFile)
+    subtitlesReader = new SubtitlesReader(subtitlesFile)
     subtitlesReader.loadFile()
     
     $("#watch-form").slideUp()
     $("#watch-video").slideDown()
+}
+
+function debug() {
+    //console.log($("#video").get(0).currentTime);
+    console.log(subtitlesReader.getCurrentLine($("#video").get(0).currentTime));
 }
